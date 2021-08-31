cd64f1e29649bf43c270bf076a9d666aed13c369
kawpls <jmugaldeb@gmail.com>
2021-08-31 15:30:04 -0500

    Create

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./**/*.{html,js}"],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
